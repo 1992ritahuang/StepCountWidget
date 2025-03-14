09637d59c67b995aef8a5b26b13316c925980335
Rita Huang <1992ritahuang@gmail.com>
2025-03-14 13:45:32 +0800

    Initial Commit

---
+++ stepCount/stepCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  stepCountApp.swift
+//  stepCount
+//
+//  Created by Rita Huang on 2025/3/14.
+//
+
+import SwiftUI
+
+@main
+struct stepCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
